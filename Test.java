69df31171d824e875414bbd2b5a1f4f321385a04
Samantha Tafoya <sammit@live.com>
2019-10-30 10:22:50 -0700

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("4 + 2 = 6");
+		//test
+
+		System.out.print("This might work");
+
+	}
+
+}
